157f29d99bb9e6122c17e2c599593fede349f79d
Pararth Shah <pararthshah717@gmail.com>
2015-05-12 17:49:52 -0700

    Fixed bug and added write-time modelling, got 67% word accuracy

--- memnn_theano_v2.py
+++ memnn_theano_v2.py
@@ -12,7 +12,7 @@ class MemNN:
         self.margin = margin
         self.n_epochs = n_epochs
         self.n_words = n_words
-        self.n_D = 3 * self.n_words
+        self.n_D = 3 * self.n_words + 3
 
         self.word_to_id = word_to_id
         self.id_to_word = dict((v, k) for k, v in word_to_id.iteritems())
@@ -21,12 +21,12 @@ class MemNN:
         phi_x = T.vector('phi_x')
 
         # True statements
-        phi_f1 = T.vector('phi_f1')
-        phi_f2 = T.vector('phi_f2')
+        phi_f1_1 = T.vector('phi_f1_1')
+        phi_f2_1 = T.vector('phi_f2_1')
 
         # False statements
-        phi_f1bar = T.vector('phi_f1bar')
-        phi_f2bar = T.vector('phi_f2bar')
+        phi_f1_2 = T.vector('phi_f1_2')
+        phi_f2_2 = T.vector('phi_f2_2')
 
         # Supporting memories
         phi_m0 = T.vector('phi_m0')
@@ -41,7 +41,7 @@ class MemNN:
         self.U_O = init_shared_normal(n_embedding, self.n_D, 0.01)
         self.U_R = init_shared_normal(n_embedding, self.n_D, 0.01)
 
-        cost = self.calc_cost(phi_x, phi_f1, phi_f1bar, phi_f2, phi_f2bar, phi_m0, phi_m1, phi_r, phi_rbar)
+        cost = self.calc_cost(phi_x, phi_f1_1, phi_f1_2, phi_f2_1, phi_f2_2, phi_m0, phi_m1, phi_r, phi_rbar)
         params = [self.U_O, self.U_R]
         gradient = T.grad(cost, params)
 
@@ -50,7 +50,7 @@ class MemNN:
             updates.append((param, param - gparam * self.lr))
 
         self.train_function = theano.function(
-            inputs = [phi_x, phi_f1, phi_f1bar, phi_f2, phi_f2bar, phi_m0, phi_m1, phi_r, phi_rbar],
+            inputs = [phi_x, phi_f1_1, phi_f1_2, phi_f2_1, phi_f2_2, phi_m0, phi_m1, phi_r, phi_rbar],
             outputs = cost,
             updates = updates)
 
@@ -63,47 +63,59 @@ class MemNN:
         score_r = self.calc_score_r(phi_x, phi_f)
         self.predict_function_r = theano.function(inputs = [phi_x, phi_f], outputs = score_r)
 
-    def calc_score(self, phi_x, phi_y, U):
-        return T.dot(U.dot(phi_x), U.dot(phi_y))
-
-    def calc_score_o(self, phi_x, phi_y):
-        return self.calc_score(phi_x, phi_y, self.U_O)
+    def calc_score_o(self, phi_x, phi_y_yp_t):
+        return T.dot(self.U_O.dot(phi_x), self.U_O.dot(phi_y_yp_t))
 
     def calc_score_r(self, phi_x, phi_y):
-        return self.calc_score(phi_x, phi_y, self.U_R)
+        return T.dot(self.U_R.dot(phi_x), self.U_R.dot(phi_y))
 
-    def calc_cost(self, phi_x, phi_f1, phi_f1bar, phi_f2, phi_f2bar, phi_m0, phi_m1, phi_r, phi_rbar):
-        correct_score1 = self.calc_score_o(phi_x, phi_f1)
-        false_score1 = self.calc_score_o(phi_x, phi_f1bar)
+    # phi_f1_1 = phi_f1 - phi_f1bar + phi_t1_1
+    # phi_f1_2 = phi_f1bar - phi_f1 + phi_t1_2
+    def calc_cost(self, phi_x, phi_f1_1, phi_f1_2, phi_f2_1, phi_f2_2, phi_m0, phi_m1, phi_r, phi_rbar):
+        score1_1 = self.calc_score_o(phi_x, phi_f1_1)
+        score1_2 = self.calc_score_o(phi_x, phi_f1_2)
 
-        correct_score2 = self.calc_score_o(phi_x + phi_m0, phi_f2)
-        false_score2 = self.calc_score_o(phi_x + phi_m0, phi_f2bar)
+        score2_1 = self.calc_score_o(phi_x + phi_m0, phi_f2_1)
+        score2_2 = self.calc_score_o(phi_x + phi_m0, phi_f2_2)
 
         correct_score3 = self.calc_score_r(phi_x + phi_m0 + phi_m1, phi_r)
         false_score3 = self.calc_score_r(phi_x + phi_m0 + phi_m1, phi_rbar)
 
         cost = (
-            T.maximum(0, self.margin - correct_score1 + false_score1) +
-            T.maximum(0, self.margin - correct_score2 + false_score2) +
+            T.maximum(0, self.margin - score1_1) + T.maximum(0, self.margin + score1_2) +
+            T.maximum(0, self.margin - score2_1) + T.maximum(0, self.margin + score2_2) +
             T.maximum(0, self.margin - correct_score3 + false_score3)
         )
 
         return cost
 
-    def construct_phi(self, phi_type, bow=None, word_id=None):
+    def construct_phi(self, phi_type, bow=None, word_id=None, ids=None):
         # type 0: question (phi_x)
         # type 1: supporting memory (phi_m*)
         # type 2: candidate memory (phi_y)
         # type 3: word vector
-        assert(phi_type >= 0 and phi_type < 4)
-        phi = np.zeros((3*self.n_words,))
+        # type 4: write-time features
+        assert(phi_type >= 0 and phi_type < 5)
+        phi = np.zeros((3*self.n_words + 3,))
         if phi_type < 3:
             assert(bow != None)
             phi[phi_type*self.n_words:(phi_type+1)*self.n_words] = bow
-        else:
+        elif phi_type == 3:
             assert(word_id != None and word_id < self.n_words)
             phi[2*self.n_words + word_id] = 1
+        else:
+            assert(ids != None and len(ids) == 3)
+            if ids[0] > ids[1]: phi[3*self.n_words] = 1
+            if ids[0] > ids[2]: phi[3*self.n_words+1] = 1
+            if ids[1] > ids[2]: phi[3*self.n_words+2] = 1
         return phi
+
+    # returns (phi_y - phi_yp + phi_t)
+    def construct_wt_phi(self, index_x, index_y, index_yp, y, yp):
+        phi_y = self.construct_phi(2, bow=y)
+        phi_yp = self.construct_phi(2, bow=yp)
+        phi_t = self.construct_phi(4, ids=[index_x, index_y, index_yp])
+        return phi_y - phi_yp + phi_t
 
     def neg_sample(self, c, num):
         assert(c < num)
@@ -113,20 +125,18 @@ class MemNN:
             f = num-1
         return f
 
-    def find_m0(self, phi_x, statements, line_no, ignore=None):
+    def find_m0(self, index_x, phi_x, statements, ignore=None):
         max_score = float("-inf")
-        index_m0 = None
-        m0 = None
-        for i in xrange(line_no):
+        index_m0 = 0
+        m0 = statements[0]
+        for i in xrange(1,len(statements)):
             if ignore and i == ignore:
                 continue
 
             s = statements[i]
-            phi_s = self.construct_phi(2, bow=s)
+            phi_s = self.construct_wt_phi(index_x, i, index_m0, s, m0)
 
-            score = self.predict_function_o(phi_x, phi_s)
-            if score > max_score:
-                max_score = score
+            if self.predict_function_o(phi_x, phi_s) >= 0:
                 index_m0 = i
                 m0 = s
 
@@ -139,6 +149,7 @@ class MemNN:
             random.shuffle(questions)
             for i, question in enumerate(questions):
                 article_no = question[0]
+                article = dataset_bow[article_no]
                 line_no = question[1]
                 question_phi = question[2]
                 correct_stmts = question[4].split(' ')
@@ -150,29 +161,32 @@ class MemNN:
 
                 # The question
                 phi_x = self.construct_phi(0, bow=question_phi)
-                # Correct statement 1
-                phi_f1 = self.construct_phi(2, bow=dataset_bow[article_no][correct_stmt1])
-                # Correct statement 2
-                phi_f2 = self.construct_phi(2, bow=dataset_bow[article_no][correct_stmt2])
-                # Correct word
-                correct_word = question[3]
-                phi_r = self.construct_phi(3, word_id=correct_word)
 
                 # Find m0
-                index_m0, m0 = self.find_m0(phi_x, dataset_bow[article_no], line_no)
+                index_m0, m0 = self.find_m0(line_no, phi_x, article[:line_no])
                 phi_m0 = self.construct_phi(1, bow=m0)
 
                 # Find m1
-                index_m1, m1 = self.find_m0(phi_x + phi_m0, dataset_bow[article_no], line_no, ignore=index_m0)
+                index_m1, m1 = self.find_m0(index_m0, phi_x + phi_m0, article[:line_no], ignore=index_m0)
                 phi_m1 = self.construct_phi(1, bow=m1)
 
                 # False statement 1
-                false_stmt1 = self.neg_sample(correct_stmt1, line_no)
-                phi_f1bar = self.construct_phi(2, bow=dataset_bow[article_no][false_stmt1])
+                false_stmt1 = index_m0
+                if false_stmt1 == correct_stmt1:
+                    false_stmt1 = self.neg_sample(correct_stmt1, line_no)
+                phi_f1_1 = self.construct_wt_phi(line_no, correct_stmt1, false_stmt1, article[correct_stmt1], article[false_stmt1])
+                phi_f1_2 = self.construct_wt_phi(line_no, false_stmt1, correct_stmt1, article[false_stmt1], article[correct_stmt1])
 
                 # False statement 2
-                false_stmt2 = self.neg_sample(correct_stmt2, line_no)
-                phi_f2bar = self.construct_phi(2, bow=dataset_bow[article_no][false_stmt2])
+                false_stmt2 = index_m1
+                if false_stmt2 == correct_stmt2:
+                    false_stmt2 = self.neg_sample(correct_stmt2, line_no)
+                phi_f2_1 = self.construct_wt_phi(line_no, correct_stmt2, false_stmt2, article[correct_stmt2], article[false_stmt2])
+                phi_f2_2 = self.construct_wt_phi(line_no, false_stmt2, correct_stmt2, article[false_stmt2], article[correct_stmt2])
+
+                # Correct word
+                correct_word = question[3]
+                phi_r = self.construct_phi(3, word_id=correct_word)
 
                 # False word
                 false_word, score = self.find_word(phi_x + phi_m0 + phi_m1, verbose=False)
@@ -184,28 +198,28 @@ class MemNN:
                     print '[SAMPLE] %s\t%s' % (self.id_to_word[correct_word], self.id_to_word[false_word])
                     w, score = self.find_word(phi_x + phi_m0 + phi_m1, verbose=False)
                     print "[BEFORE] %.3f\t%.3f\t%.3f\t%.3f\tm0:%d\tm1:%d\ta:%s\ts:%.3f\tc:%s" % (
-                        self.predict_function_o(phi_x, phi_f1),
-                        self.predict_function_o(phi_x, phi_f1bar),
-                        self.predict_function_o(phi_x + phi_m0, phi_f2),
-                        self.predict_function_o(phi_x + phi_m0, phi_f2bar),
+                        self.predict_function_o(phi_x, phi_f1_1),
+                        self.predict_function_o(phi_x, phi_f1_2),
+                        self.predict_function_o(phi_x + phi_m0, phi_f2_1),
+                        self.predict_function_o(phi_x + phi_m0, phi_f2_2),
                         index_m0, index_m1,
                         self.id_to_word[w], score, self.id_to_word[correct_word]
                     )
 
-                cost = self.train_function(phi_x, phi_f1, phi_f1bar, phi_f2, phi_f2bar, phi_m0, phi_f1, phi_r, phi_rbar)
+                cost = self.train_function(phi_x, phi_f1_1, phi_f1_2, phi_f2_1, phi_f2_2, phi_m0, phi_m1, phi_r, phi_rbar)
                 costs.append(cost)
 
                 if article_no == 1 and line_no == 12:
-                    index_m0, m0 = self.find_m0(phi_x, dataset_bow[article_no], line_no)
+                    index_m0, m0 = self.find_m0(line_no, phi_x, article[:line_no])
                     phi_m0 = self.construct_phi(1, bow=m0)
-                    index_m1, m1 = self.find_m0(phi_x + phi_m0, dataset_bow[article_no], line_no, ignore=index_m0)
+                    index_m1, m1 = self.find_m0(index_m0, phi_x + phi_m0, article[:line_no], ignore=index_m0)
                     phi_m1 = self.construct_phi(1, bow=m1)
                     w, score = self.find_word(phi_x + phi_m0 + phi_m1, verbose=False)
                     print "[ AFTER] %.3f\t%.3f\t%.3f\t%.3f\tm0:%d\tm1:%d\ta:%s\ts:%.3f\tc:%s" % (
-                        self.predict_function_o(phi_x, phi_f1),
-                        self.predict_function_o(phi_x, phi_f1bar),
-                        self.predict_function_o(phi_x + phi_m0, phi_f2),
-                        self.predict_function_o(phi_x + phi_m0, phi_f2bar),
+                        self.predict_function_o(phi_x, phi_f1_1),
+                        self.predict_function_o(phi_x, phi_f1_2),
+                        self.predict_function_o(phi_x + phi_m0, phi_f2_1),
+                        self.predict_function_o(phi_x + phi_m0, phi_f2_2),
                         index_m0, index_m1,
                         self.id_to_word[w], score, self.id_to_word[correct_word]
                     )
@@ -254,9 +268,9 @@ class MemNN:
                 phi_m0 = self.construct_phi(1, statements[0])
                 phi_m1 = self.construct_phi(1, statements[0])
             else:
-                index_m0, m0 = self.find_m0(phi_x, statements, line_no)
+                index_m0, m0 = self.find_m0(line_no, phi_x, statements[:line_no])
                 phi_m0 = self.construct_phi(1, m0)
-                index_m1, m1 = self.find_m0(phi_x + phi_m0, statements, line_no, ignore=index_m0)
+                index_m1, m1 = self.find_m0(index_m0, phi_x + phi_m0, statements[:line_no], ignore=index_m0)
                 phi_m1 = self.construct_phi(1, m1)
 
                 c1 = int(question[4].split(' ')[0])
@@ -283,6 +297,6 @@ if __name__ == "__main__":
     train_dataset, train_questions, word_to_id, num_words = parse_dataset(train_file)
     test_dataset, test_questions, _, _ = parse_dataset(test_file, word_id=num_words, word_to_id=word_to_id, update_word_ids=False)
 
-    memNN = MemNN(n_words=num_words, n_embedding=100, lr=0.01, n_epochs=100, margin=1.0, word_to_id=word_to_id)
+    memNN = MemNN(n_words=num_words, n_embedding=100, lr=0.01, n_epochs=10, margin=1.0, word_to_id=word_to_id)
     memNN.train(train_dataset, train_questions)
     memNN.predict(test_dataset, test_questions)
